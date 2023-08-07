b1b661b669374f25ccd4361042c123620dde6a20
Rusla nChekina <ruslan.chekina70@gmail.com>
2023-08-07 11:10:33 +0300

    Initial commit

---
+++ src/main/java/com/tasks/executor/TaskExecutorApplication.java
@@ -0,0 +1,13 @@
+package com.tasks.executor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskExecutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskExecutorApplication.class, args);
+	}
+
+}
